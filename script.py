d25fa5726b97ef89b4ec309516539243659c5273
Indrayana <masepindrayana@gmail.com>
2021-09-17 17:43:59 +0700

    add validation when custom half-back is none

--- script.py
+++ script.py
@@ -18,13 +18,15 @@ def create_short_link():
     with open(origin_csv, 'rb') as f:
         for i, line in enumerate(f):
             if i > 0:
-                raw_data = str(line.split()[0], 'utf-8')
-                get_school = raw_data.split(",")[0]
+                raw_data = str(line, 'utf-8')
+                
+                get_school = raw_data.split(",")[0].replace(" ", "")
+                get_origin_school = raw_data.split(",")[0]
                 get_pic = raw_data.split(",")[1]
                 get_city = raw_data.split(",")[2]
                 get_classcode = raw_data.split(",")[3]
                 get_originlink = raw_data.split(",")[4]
-                cust_link = "bit.ly/"+get_school
+                cust_link = "bit.ly/"+"TOAKM_"+get_school
 
                 data_for_shortlink = {
                     "long_url": get_originlink,
@@ -44,7 +46,13 @@ def create_short_link():
                 res_to_json_backhalf = json.loads(custom_backhalf.text)
                 custom_short_link = res_to_json_backhalf.get("custom_bitlink")
 
-                csv_data.append([get_school, get_pic, get_city, get_classcode, get_originlink, custom_short_link])
+                if custom_short_link:
+                    short_link = custom_short_link
+                else:
+                    short_link = res_to_json_shorlink.get("id")
+
+                csv_data.append([get_origin_school, get_pic, get_city, get_classcode, get_originlink, short_link])
+
 
     with open(new_csv, 'w', encoding='UTF8', newline='') as f:
         writer = csv.writer(f)
